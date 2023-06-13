fe5203d4c4d80492cba9edb6f4b905bc5b564f67
trufflelover <stephaniechung1764@gmail.com>
2023-06-12 17:42:25 -0700

    lab4 is updated

---
+++ lab5/js/init.js
@@ -0,0 +1,83 @@
+// declare variables
+let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+
+// use the variables
+const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// create a function to add markers
+// function addMarker(lat,lng,title,message){
+//     // console.log(message)
+//     L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+//     createButtons(lat,lng,location)
+//     return message
+// }
+
+
+function addMarker(data){
+    const title = data["What is the name of the place?"];
+    const lat = parseFloat(data["lat"]);
+    const lng = parseFloat(data["lng"]);
+    const bestMenu = data["What is the best menu?"];
+    const shoutout = data["(optional) What is your name? Could be nick name or any name you want to be called. You will get a shoutout on the map  ex) \"Approved by Stephanie\""];
+    const message = bestMenu + (shoutout ? "<br>Recommended by " + shoutout : "");
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <p>${message}</p>`)
+    createButtons(lat, lng, title);
+    return message;
+}
+
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // create a new button
+    newButton.id = "button"+title; // give the button an id
+    newButton.className = "coolButtons"
+    newButton.innerHTML = title; // give the button text
+    newButton.setAttribute("lat",lat); 
+    newButton.setAttribute("lng",lng);
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng], mapOptions.zoom); //this will move the view to the button location
+    })
+    document.getElementById('placeForButtons').appendChild(newButton); //append the button to placeForButtons div
+
+}
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQbmdY93DI_U_PHnAT3JqcbiVNDfPys1yb1RcfGmpEj_hs2nOA1-4UNWi0pWxHU6-T8C0sshnkrC7Ma/pub?output=csv"
+
+function loadData(url){
+    Papa.parse(url, {
+        header: true,
+        download: true,
+        complete: results => processData(results)
+    })
+}
+
+function processData(results){
+    console.log(results)
+    results.data.forEach(data => {
+        console.log(data)
+        addMarker(data)
+    })
+}
+
+function toggleIframe() {
+    let contents = document.getElementById("survey");
+    let main = document.querySelector(".main");
+    if (contents.style.display === "none") {
+      contents.style.display = "block";
+      main.style.gridTemplateColumns = "1fr 1fr";
+      map.invalidateSize();
+    } else {
+      contents.style.display = "none";
+      main.style.gridTemplateColumns = "1fr";
+      map.invalidateSize();
+    }
+  
+  }
+  
+  window.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("toggleBtn").addEventListener("click", toggleIframe);
+  });
+
+toggleIframe()
+loadData(dataUrl)
